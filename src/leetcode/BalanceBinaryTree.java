c61c324eba5f834861611b968f9c4df0dc2cfcdc
Datartvinci <461783265@qq.com>
2018-03-16 00:58:05 +0800

    BalanceBinaryTree

---
+++ src/leetcode/BalanceBinaryTree.java
@@ -0,0 +1,72 @@
+package leetcode;
+
+import codeinterview.datastructure.TreeNode;
+
+/**
+ * Given a binary tree, determine if it is height-balanced.
+ * <p>
+ * For this problem, a height-balanced binary tree is defined as:
+ * <p>
+ * a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+ * <p>
+ * Example 1:
+ * <p>
+ * Given the following tree [3,9,20,null,null,15,7]:
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * Return true.
+ * <p>
+ * Example 2:
+ * <p>
+ * Given the following tree [1,2,2,3,3,null,null,4,4]:
+ * <p>
+ * 1
+ * / \
+ * 2   2
+ * / \
+ * 3   3
+ * / \
+ * 4   4
+ * Return false.
+ */
+public class BalanceBinaryTree {
+
+    public boolean isBalancedByDfs(TreeNode root) {
+        return dfs(root) != -1;
+    }
+
+    public int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = dfs(root.left);
+        if (leftHeight == -1) return -1;
+        int rightHeight = dfs(root.right);
+        if (rightHeight == -1) return -1;
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+        return Math.max(leftHeight, rightHeight) + 1;
+
+    }
+
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return Math.abs(getHeight(root.left) - getHeight(root.right)) < 2 && isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    public int getHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(getHeight(root.left), getHeight(root.right)) + 1;
+    }
+
+
+}
